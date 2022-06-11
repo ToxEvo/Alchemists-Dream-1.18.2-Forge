49df0373939dae8a201272d40156ccda153690e6
ToxEvo <dawsalowy@gmail.com>
2022-06-11 10:06:49 +0100

    Update

--- src/main/java/net/toxevo/alcd/effect/complex/Volatile/VolatileEffect.java
+++ src/main/java/net/toxevo/alcd/effect/complex/Volatile/VolatileEffect.java
@@ -1,8 +1,6 @@
 package net.toxevo.alcd.effect.complex.Volatile;
 
-import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
@@ -18,18 +16,6 @@ public class VolatileEffect extends MobEffect {
     public void applyEffectTick(LivingEntity entity, int amplifier) {
         VolatileEffectAction.action(entity.level, entity.getX(), entity.getY(), entity.getZ(), entity);
         spawnVolatileParticle(entity.level, entity.getX(), entity.getY(), entity.getZ());
-        //spawnVolatileParticles(entity);
-
-    }
-
-    private void spawnVolatileParticles(LivingEntity entity) {
-        for(int i = 0; i < 360; i++) {
-            if(i % 20 == 0) {
-                entity.getLevel().addParticle(ModParticles.VOLATILE_PARTICLES.get(),
-                        entity.getX() + 0d, entity.getY() + 1, entity.getZ() + 0d,
-                        Math.cos(i) * 0.15d, 0.15d, Math.sin(i) * 0.15d);
-            }
-        }
     }
 
     public static void spawnVolatileParticle(LevelAccessor world, double x, double y, double z) {
