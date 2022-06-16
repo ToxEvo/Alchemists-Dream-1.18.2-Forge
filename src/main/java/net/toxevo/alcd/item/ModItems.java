1b122ac559f03692656ab30db2daeffc25604adc
ToxEvo <dawsalowy@gmail.com>
2022-06-16 11:43:55 +0100

    Update

--- src/main/java/net/toxevo/alcd/item/ModItems.java
+++ src/main/java/net/toxevo/alcd/item/ModItems.java
@@ -1,11 +1,13 @@
 package net.toxevo.alcd.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.toxevo.alcd.ALCD;
+import net.toxevo.alcd.entity.ModEntityTypes;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -13,6 +15,10 @@ public class ModItems {
 
     public static final RegistryObject<Item> VOLATILE_PETAL = ITEMS.register("volatile_petal",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ALCD_TAB)));
+
+    public static final RegistryObject<Item> RACCOON_SPAWN_EGG = ITEMS.register("raccoon_spawn_egg",
+            () -> new ForgeSpawnEggItem(ModEntityTypes.RACCOON,0x948e8d, 0x3b3635,
+                    new Item.Properties().tab(ModCreativeModeTab.ALCD_TAB)));
 
 
     public static void register(IEventBus eventBus) {
